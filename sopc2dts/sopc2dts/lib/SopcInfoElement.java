5be51fc04f10c53e301ea487e219cc662bd537fd
Walter Goossens <waltergoossens@home.nl>
2011-02-14 23:02:43 +0100

    sopc2dts: Remove useless todo messages
    Signed-off-by: Walter Goossens <waltergoossens@home.nl>

--- sopc2dts/sopc2dts/lib/SopcInfoElement.java
+++ sopc2dts/sopc2dts/lib/SopcInfoElement.java
@@ -20,13 +20,11 @@ public abstract class SopcInfoElement implements ContentHandler {
 	
 	public void startElement(String uri, String localName, String qName,
 			Attributes atts) throws SAXException {
-		// TODO Auto-generated method stub
 		System.out.println("Unhandled SopcInfo element: " + localName);
 	}
 
 	public void endElement(String uri, String localName, String qName)
 			throws SAXException {
-		// TODO Auto-generated method stub
 		if(localName.equalsIgnoreCase(getElementName()))
 		{
 			if((xmlReader!=null)&&(parentElement!=null))
@@ -39,51 +37,33 @@ public abstract class SopcInfoElement implements ContentHandler {
 
 	public void characters(char[] ch, int start, int length)
 			throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void endDocument() throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void endPrefixMapping(String prefix) throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void ignorableWhitespace(char[] ch, int start, int length)
 			throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void processingInstruction(String target, String data)
 			throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void setDocumentLocator(Locator locator) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void skippedEntity(String name) throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void startDocument() throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void startPrefixMapping(String prefix, String uri)
 			throws SAXException {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
