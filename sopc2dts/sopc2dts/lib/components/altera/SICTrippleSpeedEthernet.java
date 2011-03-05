6ee401181c5ffc1b22360b1f38a5c100625f767a
Walter Goossens <waltergoossens@home.nl>
2011-03-05 22:08:32 +0100

    sopc2dts: AlterTSE: Handle missing sgdma engines instead of crashing
    Signed-off-by: Walter Goossens <waltergoossens@home.nl>

--- sopc2dts/sopc2dts/lib/components/altera/SICTrippleSpeedEthernet.java
+++ sopc2dts/sopc2dts/lib/components/altera/SICTrippleSpeedEthernet.java
@@ -3,6 +3,7 @@ package sopc2dts.lib.components.altera;
 import sopc2dts.generators.AbstractSopcGenerator;
 import sopc2dts.lib.BoardInfo;
 import sopc2dts.lib.Connection;
+import sopc2dts.lib.components.Interface;
 import sopc2dts.lib.components.SopcComponentDescription;
 import sopc2dts.lib.components.BasicComponent;
 import sopc2dts.lib.components.base.SICEthernet;
@@ -13,15 +14,35 @@ public class SICTrippleSpeedEthernet extends SICEthernet {
 		super(scd, iName, version);
 	}
 
+	private String getSGDMAEngine(int indentLevel, Interface intf, String dtsEntry)
+	{
+		String res = "";
+		if((intf!=null) && (intf.getConnections().size()>0))
+		{
+			BasicComponent comp = null;
+			if(intf.isMaster())
+			{
+				comp = intf.getConnections().firstElement().getSlaveModule();
+			} else {
+				comp = intf.getConnections().firstElement().getMasterModule();
+			}
+			if(comp != null)
+			{
+				res += AbstractSopcGenerator.indent(indentLevel) + dtsEntry + 
+									" = <&" + comp.getInstanceName() + ">;\n";
+			}
+		}
+		if(res.length()==0)
+		{
+			res = AbstractSopcGenerator.indent(indentLevel) + 
+					"//Port " + intf.getName() + " seems not connected\n";
+		}
+		return res;
+	}
 	@Override
 	public String toDtsExtras(BoardInfo bi, int indentLevel, Connection conn, Boolean endComponent)
 	{
-		String res = "";
-		BasicComponent compTX, compRX;
-		compTX = getInterfaceByName("transmit").getConnections().get(0).getMasterModule();
-		compRX = getInterfaceByName("receive").getConnections().get(0).getSlaveModule();
-		res += AbstractSopcGenerator.indent(indentLevel) + "ALTR,sgdma_tx = <&" + compTX.getInstanceName() + ">;\n";
-		res += AbstractSopcGenerator.indent(indentLevel) + "ALTR,sgdma_rx = <&" + compRX.getInstanceName() + ">;\n";
-		return res;
+		return getSGDMAEngine(indentLevel, getInterfaceByName("transmit"), "ALTR,sgdma_tx") +
+				getSGDMAEngine(indentLevel, getInterfaceByName("receive"), "ALTR,sgdma_rx");
 	}
 }
