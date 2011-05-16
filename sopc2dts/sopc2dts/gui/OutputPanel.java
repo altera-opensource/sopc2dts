aff44387c82a8ffa43db241a54f8bfc1315cfde9
Walter Goossens <waltergoossens@home.nl>
2011-05-16 23:20:00 +0200

    sopc2dts: Add missing copyright stuff to the new files
    Signed-off-by: Walter Goossens <waltergoossens@home.nl>

--- sopc2dts/sopc2dts/gui/OutputPanel.java
+++ sopc2dts/sopc2dts/gui/OutputPanel.java
@@ -1,3 +1,22 @@
+/*
+sopc2dts - Devicetree generation for Altera systems
+
+Copyright (C) 2011 Walter Goossens <waltergoossens@home.nl>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 package sopc2dts.gui;
 
 import java.awt.BorderLayout;
@@ -10,7 +29,6 @@ import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -20,8 +38,7 @@ import sopc2dts.lib.AvalonSystem;
 import sopc2dts.lib.BoardInfo;
 
 public class OutputPanel extends JPanel implements ActionListener {
-	private static final long serialVersionUID = -5350181042150149050L;
-
+	private static final long serialVersionUID = 8995906903008952772L;
 	JTextArea txtDts = new JTextArea();
 	JButton saveDTS = new JButton("Save as DTS");
 	AvalonSystem sys;
