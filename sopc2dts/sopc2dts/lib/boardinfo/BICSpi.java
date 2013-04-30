5a993ae8b1e1844f6c13f8b179f072e147c88852
Walter Goossens <waltergoossens@home.nl>
2013-04-30 21:07:31 +0200

    boardinfo: fix bug in SPI master components

--- sopc2dts/sopc2dts/lib/boardinfo/BICSpi.java
+++ sopc2dts/sopc2dts/lib/boardinfo/BICSpi.java
@@ -1,7 +1,7 @@
 /*
 sopc2dts - Devicetree generation for Altera systems
 
-Copyright (C) 2011 Walter Goossens <waltergoossens@home.nl>
+Copyright (C) 2011 - 2013 Walter Goossens <waltergoossens@home.nl>
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -36,7 +36,7 @@ public class BICSpi extends BoardInfoComponent {
 
 	@Override
 	public String getXml() {
-		String res = "\t<" + TAG_NAME + "=\"" + instanceName + "\">\n";
+		String res = "\t<" + TAG_NAME + " name=\"" + instanceName + "\">\n";
 		for(SpiSlave slave : vSlaves)
 		{
 			res += slave.getXml();
